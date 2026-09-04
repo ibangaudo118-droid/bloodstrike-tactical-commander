82d666de5b437cab025eb461a612e06493ce04b0
ibangaudo118-droid <ibangaudo118@gmail.com>
2026-09-04 19:15:52 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,9 +23,11 @@ android {
 
 dependencies {
     implementation(platform("io.github.jan-tennert.supabase:bom:3.0.0"))
+
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:functions-kt")
 
     implementation("io.ktor:ktor-client-android:3.2.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
